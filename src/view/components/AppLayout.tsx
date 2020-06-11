5b663e0afb2580eff021f45ba38e6c6bbe5982d8
ouyangzeyu <243175186@qq.com>
2020-06-11 22:46:41 +0800

    更新组件样式

--- src/view/components/AppLayout.tsx
+++ src/view/components/AppLayout.tsx
@@ -26,10 +26,10 @@ const AppLayout = (): React.FunctionComponentElement<unknown> => {
                 <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
                     <Menu.Item key="1" icon={<PieChartOutlined />}>
                         Option 1
-                </Menu.Item>
+                    </Menu.Item>
                     <Menu.Item key="2" icon={<DesktopOutlined />}>
                         Option 2
-                </Menu.Item>
+                    </Menu.Item>
                     <SubMenu key="sub1" icon={<UserOutlined />} title="User">
                         <Menu.Item key="3">Tom</Menu.Item>
                         <Menu.Item key="4">Bill</Menu.Item>
